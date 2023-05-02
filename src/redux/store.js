dc97a6676c269b3b8f722099d65e651c3fe0c47f
RudenkoOV <113025388+RudenkoOV@users.noreply.github.com>
2023-05-02 13:18:06 +0300

    VV7

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,39 @@
-import { configureStore, createReducer } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './addContactsSlice';
+import { filterReducer } from './filterSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
 
-// import { rootReducer } from "./reducer";
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filterReducer,
+});
 
-const myReducer = createReducer(1000, {});
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: [],
-    filter: myReducer
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
